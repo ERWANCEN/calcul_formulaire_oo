b4bc9b3ecb9d1acba2bb9ee5afb798c477b222c9
ERWANCEN <erwance@hotmail.fr>
2024-11-10 23:34:09 +0100

    commentaires du fichier classe CalculFormulaire

--- CalculFormulaire.php
+++ CalculFormulaire.php
@@ -1,15 +1,41 @@
 <?php
 
+// je crée une classe que j'appelle 'CalculFormulaire'
 class CalculFormulaire
 {
+    /**
+     * Propriété qui s'affichera accompagnée de la réponse si tout est correct
+     *
+     * @var string
+     */
     private string $_resultat = "<p class=\"alert alert-primary\">Le résultat de votre opération est : ";
+
+    /**
+     * Propriété qui va stocker les erreurs si il y en a
+     *
+     * @var string
+     */
     private string $_error = "";
+
+    /**
+     * Méthode qui permet de transformer chaque élément du tableau reçu en variables ainsi que les envoyer au commutateur pour vérifier les potentielles erreurs et préparer la solution
+     *
+     * @param array $post
+     */
     public function __construct(array $post)
     {
         $extractPost = extract($post);
         $this->setResultat($val1, $operator, $val2);
     }
 
+    /**
+     * Vérifie les potentielles erreurs et prépare la solution
+     *
+     * @param integer $val1
+     * @param string $operator
+     * @param integer $val2
+     * @return void
+     */
     public function setResultat(int $val1, string $operator, int $val2)
     {
         if (!is_int($val1) || !is_int($val2)) {
@@ -33,6 +59,11 @@ class CalculFormulaire
         }
     }
 
+    /**
+     * Permet de retourner soit les erreurs, soit la réponse
+     *
+     * @return void
+     */
     public function getResultat()
     {
         if (!empty($this->_error)) {
